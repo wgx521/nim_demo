7faf688dabce9fe95ee9d0a1c8b4289f1305fff4
wgx521 <wgx521@139.com>
2018-01-19 17:59:57 +0800

    Initial commit

---
+++ demo/build/generated/source/buildConfig/debug/com/netease/nim/demo/BuildConfig.java
@@ -0,0 +1,16 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.netease.nim.demo;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.netease.nim.demo";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 42;
+  public static final String VERSION_NAME = "4.7.1";
+  // Fields from default config.
+  public static final String BUILD_DATE = "2018-1-19 17:19:50";
+  public static final String GIT_REVISION = "";
+}
